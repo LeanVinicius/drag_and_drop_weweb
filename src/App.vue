7c35cd878430705fee5a8f0d99161e821267e82c
LeanVinicius <vinicius.mourao@leansolutionsbr.onmicrosoft.com>
2025-02-18 16:31:00 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import FlowchartEditor from './components/FlowchartEditor.vue'
+</script>
+
+<template>
+  <FlowchartEditor />
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
